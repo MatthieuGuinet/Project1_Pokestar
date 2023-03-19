7c1fcc9deec38fa06df17484124c961d154fdfe7
Gaspard Caillaud <gaspard.caillaud@gmail.com>
2023-03-19 01:08:32 +0100

    compte du nombre de points de chaque pokemon du quizz selon les choix de l'internaute, modification des classes de nom pokemon à chaque clic suivant

--- quizz.js
+++ quizz.js
@@ -35,9 +35,16 @@ let question3 = {
 // defining the var counting number of "suivant" clicks, value is the current question
 let suivantCount = 0;
 
+//defining the var count of each pokemon
+let eeveeCount = 0;
+let flareonCount = 0;
+let jolteonCount = 0;
+let vaporeonCount = 0;
+
 // function to define the "suivant" button effect in the quizz
 
 export default function nextQuestion() {
+  let allAnswers = document.getElementsByClassName("quizz-answer");
   // controls that an answer is effectively chosen, then adds 1 to the suivantCount
   if (
     answer1.classList.contains("isChosen") ||
@@ -46,35 +53,70 @@ export default function nextQuestion() {
     answer4.classList.contains("isChosen")
   ) {
     suivantCount += 1;
-    let allAnswers = document.getElementsByClassName ("quizz-answer");
+
+    // takes into account the answer for the final result
+
+    let chosenAnswer = document.getElementsByClassName("isChosen");
+    if (chosenAnswer[0].classList.contains("eevee")) {
+      eeveeCount += 1;
+    }
+    if (chosenAnswer[0].classList.contains("flareon")) {
+      flareonCount += 1;
+    }
+    if (chosenAnswer[0].classList.contains("jolteon")) {
+      jolteonCount += 1;
+    }
+    if (chosenAnswer[0].classList.contains("vaporeon")) {
+      vaporeonCount += 1;
+    }
+
+    // removes the isChosen class  from all the items before switching to the next question
+
+    // let allAnswers = document.getElementsByClassName("quizz-answer");
+
     for (let i = 0; i < allAnswers.length; i++) {
-      if (allAnswers[i].classList.contains("isChosen")) {
-        allAnswers[i].classList.toggle("isChosen");
-      }
-      //  controls that all the questions aren't answered yet
-      switch (suivantCount) {
-        case 1:
-          quizzQuestion.innerText = question2.question;
-          answer1.innerText = question2.answer1;
-          answer2.innerText = question2.answer2;
-          answer3.innerText = question2.answer3;
-          answer4.innerText = question2.answer4;
+      allAnswers[i].classList.remove("isChosen");
+      allAnswers[i].classList.remove("eevee");
+      allAnswers[i].classList.remove("flareon");
+      allAnswers[i].classList.remove("jolteon");
+      allAnswers[i].classList.remove("vaporeon");
+    }
 
-          break;
+    //removes the pokemon named class
 
-        case 2:
-          quizzQuestion.innerText = question3.question;
-          answer1.innerText = question3.answer1;
-          answer2.innerText = question3.answer2;
-          answer3.innerText = question3.answer3;
-          answer4.innerText = question3.answer4;
+    //  controls that all the questions aren't answered yet and displays answers for the right question
+    switch (suivantCount) {
+      case 1:
+        quizzQuestion.innerText = question2.question;
+        answer1.innerText = question2.answer1;
+        answer2.innerText = question2.answer2;
+        answer3.innerText = question2.answer3;
+        answer4.innerText = question2.answer4;
+        answer1.classList.add(question2.answer1class);
+        answer2.classList.add(question2.answer2class);
+        answer3.classList.add(question2.answer3class);
+        answer4.classList.add(question2.answer4class);
+        break;
 
-          break;
-        case 3:
+      case 2:
+        quizzQuestion.innerText = question3.question;
+        answer1.innerText = question3.answer1;
+        answer2.innerText = question3.answer2;
+        answer3.innerText = question3.answer3;
+        answer4.innerText = question3.answer4;
+        answer1.classList.add(question3.answer1class);
+        answer2.classList.add(question3.answer2class);
+        answer3.classList.add(question3.answer3class);
+        answer4.classList.add(question3.answer4class);
 
-        default:
-          break;
-      }
+        break;
+
+      // all the questions have been answered, let's display some result depending on the previous choices associated to some pokemon
+      case 3:
+        console.log(eeveeCount);
+
+      default:
+        break;
     }
   }
 }
