79927f7b2d5def0e087d24eec50badb13322134c
Jasmine Grozinger <jasmine.grozinger@gmail.com>
2023-03-20 14:14:27 +0100

    Update navbar

--- index.js
+++ index.js
@@ -2,31 +2,21 @@
 const burgerIcon = document.getElementById("burger_icon");
 const burgerMenu = document.getElementById("nav_links");
 const burger = document.getElementById("burger")
-const navBurger = document.getElementsByClassName("navBurger")
 
-burgerIcon.addEventListener('click', function () {
+function closeNav() {
     burgerMenu.classList.toggle("burger_open");
     burger.classList.toggle("animationOpen");
-})
+}
 
-// disparition du menu burger au click sur les liens
-const navLegend = document.getElementById("nav_legend");
-const navLocalisation = document.getElementById("nav_localisation");
-const navCaracteristics = document.getElementById("nav_caracteristics");
-const navQuizz = document.getElementById("nav_quizz");
-navLegend.addEventListener('click', function () {
-    burgerMenu.classList.remove("burger_open");
-    burger.classList.remove("animationOpen");
+burgerIcon.addEventListener('click', function () {
+    closeNav()
 })
-navLocalisation.addEventListener('click', function () {
-    burgerMenu.classList.remove("burger_open");
-    burger.classList.remove("animationOpen");
-})
-navCaracteristics.addEventListener('click', function () {
-    burgerMenu.classList.remove("burger_open");
-    burger.classList.remove("animationOpen");
-})
-navQuizz.addEventListener('click', function () {
-    burgerMenu.classList.remove("burger_open");
-    burger.classList.remove("animationOpen");
-})
+// fermeture du menu burger après click sur un lien
+const navElement = document.getElementsByClassName("nav");
+
+for (let i = 0; i < navElement.length; i++) {
+    const link = navElement[i];
+    link.addEventListener("click", () => {
+        closeNav()
+    })
+}
