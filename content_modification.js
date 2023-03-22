79743ec3fdc209c24eccae11f7245ae6382e53f2
Gaspard Caillaud <gaspard.caillaud@gmail.com>
2023-03-22 17:24:53 +0100

    début du remplissage de contenu, pas encore modifié dans HTML

--- content_modification.js
+++ content_modification.js
@@ -35,13 +35,14 @@ class PokemonContent {
 replaceContent(){
     document.getElementsByClassName("pokemon-name-mobile")[0].textContent = `${this.pokemonNameCapitalCase}`;
     document.getElementsByClassName("pokemon-name")[0].textContent = `${this.pokemonNameCapitalCase}`;
+    
     document.getElementById("pokemon_left").textContent = `${this.headerLeftName}`;
     document.getElementById("pokemon_right").textContent = `${this.headerRightName}`;  
     // document.getElementById("header-type").textContent = `${this.pokemonType}`;
     // document.getElementById("little-description").textContent = `${this.littleDescription}`;
     document.getElementById("hero-header-img").src = `${this.mainImgSrc}`;
-    // document.getElementById("legend-text").textContent = `${this.legend}`;
-    // document.getElementById("localisation-text").textContent = `${this.locationInstructions}`;
+    document.getElementById("legend-text").textContent = `${this.legend}`;
+    document.getElementById("localisation-text").textContent = `${this.locationInstructions}`;
     // document.getElementById("map-image").src = `${this.mapImgSrc}`;
     // document.getElementById("caracteristics-text").textContent = `${this.caracteristics}`;
     // // idem pour type, weakness, attacks, etc... mettre les bons IDs
@@ -54,10 +55,10 @@ replaceContent(){
 
 // declaration of the four pokemon contents 
 
-const eeveeContent = new PokemonContent("EVOLI","#3a3129","#c5915d","AQUALI","PYROLI","FEU","Roux, les yeux bleus, pour 0.58m de brasier","assets\\EeveePokeball-img.png","C'est un grand grand guerrier de l'apocalypse","Tu le trouveras en cherchant bien","assets\\aqua_nav.png", "Il a plein de caractéristiques super chouettes","assets\\pins map.png", "blabla","blabla","blabla","blabla","blabla");
-const flareonContent = new PokemonContent("PYROLI","#ffa948","#fb7c26","EVOLI","VOLTALI","FEU","Roux, les yeux bleus, pour 0.58m de brasier","assets\\Flareon_Pokeball.png","C'est un grand grand guerrier de l'apocalypse","Tu le trouveras en cherchant bien","assets\\aqua_nav.png", "Il a plein de caractéristiques super chouettes","assets\\pins map.png", "blabla","blabla","blabla","blabla","blabla");
-const jolteonContent = new PokemonContent("VOLTALI","#e9bc2e","#b9812b","PYROLI","AQUALI","ELECTRIQUE","Roux, les yeux bleus, pour 0.58m de brasier","assets\\Volteon_Pokeball.png","C'est un grand grand guerrier de l'apocalypse","Tu le trouveras en cherchant bien","assets\\aqua_nav.png", "Il a plein de caractéristiques super chouettes","assets\\pins map.png", "blabla","blabla","blabla","blabla","blabla");
-const vaporeonContent = new PokemonContent("AQUALI","#879cc6","#3a5d83","VOLTALI","EVOLI","EAU","Roux, les yeux bleus, pour 0.58m de brasier","assets\\Vaporeon_Pokeball.png","C'est un grand grand guerrier de l'apocalypse","Tu le trouveras en cherchant bien","assets\\aqua_nav.png", "Il a plein de caractéristiques super chouettes","assets\\pins map.png", "blabla","blabla","blabla","blabla","blabla");
+const eeveeContent = new PokemonContent("EVOLI","#3a3129","#c5915d","AQUALI","PYROLI","FEU","Brun, les yeux marrons, pour 0.63m de douceur","assets\\EeveePokeball-img.png",legends.eeveeLegend,locations.eeveeLocation,"assets\\aqua_nav.png", "Il a plein de caractéristiques super chouettes","assets\\pins map.png", "blabla","blabla","blabla","blabla","blabla");
+const flareonContent = new PokemonContent("PYROLI","#ffa948","#fb7c26","EVOLI","VOLTALI","FEU","Roux, les yeux bleus, pour 0.58m de brasier","assets\\Flareon_Pokeball.png",legends.flareonLegend,locations.flareonLocation,"assets\\aqua_nav.png", "Il a plein de caractéristiques super chouettes","assets\\pins map.png", "blabla","blabla","blabla","blabla","blabla");
+const jolteonContent = new PokemonContent("VOLTALI","#e9bc2e","#b9812b","PYROLI","AQUALI","ELECTRIQUE","Roux, les yeux bleus, pour 0.58m de brasier","assets\\Volteon_Pokeball.png",legends.jolteonLegend,locations.jolteonLocation,"assets\\aqua_nav.png", "Il a plein de caractéristiques super chouettes","assets\\pins map.png", "blabla","blabla","blabla","blabla","blabla");
+const vaporeonContent = new PokemonContent("AQUALI","#879cc6","#3a5d83","VOLTALI","EVOLI","EAU","Roux, les yeux bleus, pour 0.58m de brasier","assets\\Vaporeon_Pokeball.png",legends.vaporeonLegend,locations.vaporeonLocation,"assets\\aqua_nav.png", "Il a plein de caractéristiques super chouettes","assets\\pins map.png", "blabla","blabla","blabla","blabla","blabla");
 
 
 export let switchPokemon = {
